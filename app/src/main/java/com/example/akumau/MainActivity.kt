8b15d83aa9a5e1e6e1896d370cb04bdee0fce68c
azuhalz <ahmadzuhal6@gmail.com>
2023-06-04 09:17:59 +0700

    Menambahkan item Latihan soal dan Tryout di Bottom Navigation

--- app/src/main/java/com/example/akumau/MainActivity.kt
+++ app/src/main/java/com/example/akumau/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_latihan_soal, R.id.navigation_try_out
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
