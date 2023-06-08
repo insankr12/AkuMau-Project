cabf0fdee28a8b340d44352e2dfe681cde3eeb7c
insankr12 <123754766+insankr12@users.noreply.github.com>
2023-06-08 19:15:53 +0700

    Update routes.js

--- Cloud Computing/login-page/routes.js
+++ Cloud Computing/login-page/routes.js
@@ -2,12 +2,22 @@
 
 const express = require('express');
 const router = express.Router();
-const { handleRegistration, handleLogin } = require('./handler');
+const { handleRegistration, handleLogin, handleTermsAndConditions, handleOTPVerification, handleTopikMateri, handleSubBabByTopik } = require('./handler');
 
 // Rute untuk registrasi
 router.post('/register', handleRegistration);
 
 // Rute untuk login
 router.post('/login', handleLogin);
+
+// Rute untuk halaman S&K
+router.get('/terms', handleTermsAndConditions);
+
+// Rute untuk memverifikasi OTP
+router.post('/verification', handleOTPVerification);
+
+// Rute untuk pemilihan materi
+router.get('/topik', handleTopikMateri);
+router.get('/subbab/:topik', handleSubBabByTopik);
 
 module.exports = router;
