15c3edc9b1b783cfc3eec94952f7c459aea607e7
insankr12 <123754766+insankr12@users.noreply.github.com>
2023-06-08 19:15:09 +0700

    Update handler.js

--- Cloud Computing/login-page/handler.js
+++ Cloud Computing/login-page/handler.js
@@ -1,21 +1,48 @@
 // handler.js
 
-const { addUser, getUserByUsername } = require('./database');
+const { addUser, getUserByUsername, generateOTP, sendEmail, getTermsAndConditions, getTopikMateri, getSubBabByTopik } = require('./database');
 
 // Fungsi untuk menangani permintaan registrasi
 function handleRegistration(req, res) {
-  const { username, email, password } = req.body;
+  const { username, email, password, agree } = req.body;
 
-  // Lakukan validasi username dan password
+  // Lakukan validasi username, email, dan password
   if (!username || !email || !password) {
     res.status(400).json({ message: 'Username, email, dan password harus diisi' });
+  } else if (!agree){
+    res.status(403).json({ message: 'Anda harus menyetujui S&K' });
   } else if (getUserByUsername(username)) {
     res.status(409).json({ message: 'Username sudah terdaftar' });
   } else {
-    // Tambahkan pengguna ke database
-    addUser(username, email, password);
+    // Generate OTP
+    const otp = generateOTP();
 
-    res.status(201).json({ message: 'Registrasi berhasil' });
+    // Simpan pengguna ke database dengan status unverified dan OTP
+    addUser(username, email, password, otp, false, agree);
+
+    // Kirim email verifikasi
+    sendEmail(email, 'Verifikasi Akun', `Kode OTP Anda: ${otp}`);
+
+    res.status(201).json({ message: 'Registrasi berhasil. Silakan cek email untuk kode OTP' });
+  }
+}
+
+// Fungsi untuk menangani permintaan verifikasi OTP
+function handleOTPVerification(req, res) {
+  const { username, otp } = req.body;
+  const user = getUserByUsername(username);
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found' });
+  } else {
+    const isOTPValid = verifyOTP(username, otp);
+
+    if (isOTPValid) {
+      updateUserVerificationStatus(username, true); // Update user verification status to true
+      res.status(200).json({ message: 'Account successfully verified' });
+    } else {
+      res.status(400).json({ message: 'Invalid OTP' });
+    }
   }
 }
 
@@ -26,14 +53,43 @@ function handleLogin(req, res) {
   // Cari pengguna berdasarkan username
   const user = getUserByUsername(username);
 
-  if (user && user.password === password) {
+  if (user && user.password === password && user.verified) {
     res.status(200).json({ message: 'Login berhasil' });
+  } else if (user && user.password === password && !user.verified) {
+    res.status(401).json({ message: 'Akun belum diverifikasi. Silakan cek email untuk kode OTP' });
   } else {
     res.status(401).json({ message: 'Username atau password salah' });
+  }
+}
+
+// Fungsi untuk menampilkan halaman S&K
+function handleTermsAndConditions(req, res) {
+  const termsAndConditions = getTermsAndConditions();
+  res.render('terms', { termsAndConditions });
+}
+
+// Fungsi untuk menampilkan materi
+function handleTopikMateri(req, res) {
+  const topikMateri = getTopikMateri();
+  res.json(topikMateri);
+}
+
+function handleSubBabByTopik(req, res) {
+  const { topik } = req.params;
+  const subBab = getSubBabByTopik(topik);
+
+  if (subBab) {
+    res.json(subBab);
+  } else {
+    res.status(404).json({ message: 'Sub-bab not found' });
   }
 }
 
 module.exports = {
   handleRegistration,
   handleLogin,
+  handleTermsAndConditions,
+  handleOTPVerification,
+  handleTopikMateri,
+  handleSubBabByTopik,
 };
