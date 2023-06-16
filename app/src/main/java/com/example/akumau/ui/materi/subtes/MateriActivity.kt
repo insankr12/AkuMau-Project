2b06520655d8eb626fc03db1dc643f444b8c26c7
azuhalz <ahmadzuhal6@gmail.com>
2023-06-16 20:27:34 +0700

    Membuat materi activity di folder Sub tes

---
+++ app/src/main/java/com/example/akumau/ui/materi/subtes/MateriActivity.kt
@@ -0,0 +1,12 @@
+package com.example.akumau.ui.materi.subtes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.akumau.R
+
+class MateriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materi)
+    }
+}
