1cb40ddb44deab1c5ce789aa5d675a105452a4fb
azuhalz <ahmadzuhal6@gmail.com>
2023-06-16 20:26:37 +0700

    Membuat Konten materi activity

---
+++ app/src/main/java/com/example/akumau/ui/materi/konten/KontenMateriActivity.kt
@@ -0,0 +1,12 @@
+package com.example.akumau.ui.materi.konten
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.akumau.R
+
+class KontenMateriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konten_materi)
+    }
+}
