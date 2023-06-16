ca0639414f0603e0816b1b887906838e658c49bf
azuhalz <ahmadzuhal6@gmail.com>
2023-06-16 20:25:40 +0700

    Membuat Bab activity dan Bab adapter

---
+++ app/src/main/java/com/example/akumau/ui/materi/bab/BabActivity.kt
@@ -0,0 +1,12 @@
+package com.example.akumau.ui.materi.bab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.akumau.R
+
+class BabActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bab)
+    }
+}
---
+++ app/src/main/java/com/example/akumau/ui/materi/bab/BabAdapter.kt
@@ -0,0 +1,2 @@
+package com.example.akumau.ui.materi.bab
+
