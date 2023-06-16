aa0a5864d65dd559d8423c07fab64e65170146ec
azuhalz <ahmadzuhal6@gmail.com>
2023-06-16 22:08:02 +0700

    Mengisi kode bab activity dan bab adapter untuk bab

--- app/src/main/java/com/example/akumau/ui/materi/bab/BabActivity.kt
+++ app/src/main/java/com/example/akumau/ui/materi/bab/BabActivity.kt
@@ -2,11 +2,24 @@ package com.example.akumau.ui.materi.bab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.akumau.R
+import com.example.akumau.data.response.MateriItem
+import com.example.akumau.data.response.MateriResponseItem
+import com.example.akumau.databinding.ActivityBabBinding
 
 class BabActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBabBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bab)
+        binding = ActivityBabBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val item = intent.getParcelableExtra<MateriResponseItem>("MATERI_RESPONSE_ITEM")
+        binding.rvBab.setHasFixedSize(true)
+        binding.rvBab.layoutManager = LinearLayoutManager(this)
+        val adapter = BabAdapter(item?.materi as ArrayList<MateriItem>)
+        binding.rvBab.adapter = adapter
     }
 }
--- app/src/main/java/com/example/akumau/ui/materi/bab/BabAdapter.kt
+++ app/src/main/java/com/example/akumau/ui/materi/bab/BabAdapter.kt
@@ -1,2 +1,43 @@
 package com.example.akumau.ui.materi.bab
 
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.akumau.R
+import com.example.akumau.data.response.MateriItem
+import com.example.akumau.ui.materi.konten.KontenMateriActivity
+
+class BabAdapter(private val list: ArrayList<MateriItem>) : RecyclerView
+.Adapter<BabAdapter.BabHolder>() {
+
+    class BabHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(materiItem: MateriItem){
+            with(itemView){
+                val tvItem: TextView = itemView.findViewById(R.id.tvItem)
+                tvItem.text = materiItem.judul
+                setOnClickListener {
+                    val context = it.context
+                    val intent = Intent(context, KontenMateriActivity::class.java)
+                    intent.putExtra("MATERI_ITEM", materiItem)
+                    context.startActivity(intent)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BabAdapter.BabHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(
+            R.layout.item,
+            parent, false)
+        return BabAdapter.BabHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BabAdapter.BabHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+}
