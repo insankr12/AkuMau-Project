df361b4469dac7a8feab4e5084ce485192b440ce
azuhalz <ahmadzuhal6@gmail.com>
2023-06-16 22:08:51 +0700

    Mengisi kode untuk konten materi activity

--- app/src/main/java/com/example/akumau/ui/materi/konten/KontenMateriActivity.kt
+++ app/src/main/java/com/example/akumau/ui/materi/konten/KontenMateriActivity.kt
@@ -3,10 +3,21 @@ package com.example.akumau.ui.materi.konten
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.akumau.R
+import com.example.akumau.data.response.MateriItem
+import com.example.akumau.databinding.ActivityKontenMateriBinding
 
 class KontenMateriActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityKontenMateriBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_konten_materi)
+        binding = ActivityKontenMateriBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val item = intent.getParcelableExtra<MateriItem>("MATERI_ITEM")
+        binding.apply {
+            tvJudul.text = item?.judul
+            tvIsi.text = item?.isi
+        }
     }
 }
