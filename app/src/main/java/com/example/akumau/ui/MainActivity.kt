50177395017f238502c55ffda40faaba037e78da
azuhalz <ahmadzuhal6@gmail.com>
2023-06-16 20:32:08 +0700

    Mengedit layout activity_main.xml

--- app/src/main/java/com/example/akumau/ui/MainActivity.kt
+++ app/src/main/java/com/example/akumau/ui/MainActivity.kt
@@ -2,14 +2,9 @@ package com.example.akumau.ui
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.akumau.R
 import com.example.akumau.databinding.ActivityMainBinding
+import com.example.akumau.ui.materi.subtes.MateriActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
